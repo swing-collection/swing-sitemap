597f8606c2031560c7829f50c12864805c482769
Lars van Vianen <lars@vianen.com>
2024-08-11 09:37:17 +0200

    Clean

--- src/swing-sitemap/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .sitemap_static import StaticSitemap
-
-
-from .sitemap import StaticViewSitemap
-from .sitemap import ProjectSitemap
-from .sitemap import StorySitemap
-
