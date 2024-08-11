f3305fa84b445f73261e93520a20e36c575028c0
Lars van Vianen <lars@vianen.com>
2024-08-11 09:37:52 +0200

    Create __init__.py

---
+++ src/swing_sitemap/utils/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from .sitemap_static import StaticSitemap
+
+
+from .sitemap import StaticViewSitemap
+from .sitemap import ProjectSitemap
+from .sitemap import StorySitemap
+
